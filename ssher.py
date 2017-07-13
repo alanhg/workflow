830d7e4aa039c22b076a0b5c40d6536d1dbf768e
alanhg <qianghe421@163.com>
2017-07-13 10:17:33 +0800

    optimize

--- ssher.py
+++ ssher.py
@@ -7,53 +7,49 @@ import paramiko
 import threading
 
 
-
-def ssh2(ip,username,passwd,cmd):
-
+def ssh2(ip, username, passwd, cmd):
     try:
 
         ssh = paramiko.SSHClient()
 
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-        ssh.connect(ip,22,username,passwd,timeout=5)
+        ssh.connect(ip, 22, username, passwd, timeout=5)
 
         for m in cmd:
 
             stdin, stdout, stderr = ssh.exec_command(m)
 
-#           stdin.write("Y")   #简单交互，输入 ‘Y’
+            #           stdin.write("Y")   #简单交互，输入 ‘Y’
 
             out = stdout.readlines()
 
-            #屏幕输出
+            # 屏幕输出
 
             for o in out:
-
                 print(o),
 
-        print('%s\tOK\n'%(ip))
+        print('%s\tOK\n' % (ip))
 
         ssh.close()
 
-    except :
+    except:
 
-        print('%s\tError\n'%(ip))
-
-ips=['192.168.1.81'] #  目标服务器-CI
+        print('%s\tError\n' % (ip))
 
 
-if __name__=='__main__':
+ips = ['192.168.1.81']  # 目标服务器-CI
 
-    cmd = ['cal','echo hello!']#你要执行的命令列表
+if __name__ == '__main__':
 
-    username = "root"  #用户名
+    cmd = ['cal', 'echo hello!']  # 你要执行的命令列表
 
-    passwd = "123456"    #密码
+    username = "root"  # 用户名
+
+    passwd = "123456"  # 密码
 
     print("Begin......")
 
     for ip in ips:
-
-        a=threading.Thread(target=ssh2,args=(ip,username,passwd,cmd))
+        a = threading.Thread(target=ssh2, args=(ip, username, passwd, cmd))
         a.start()
