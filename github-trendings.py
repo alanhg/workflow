61f562e4f940de6f859a2644b32cae71ee1d29d0
alanhg <qianghe421@163.com>
2017-08-01 17:46:01 +0800

    pip3 install beautifulsoup4

--- github-trendings.py
+++ github-trendings.py
@@ -3,7 +3,12 @@
 邮件给目标邮箱
 """
 import requests
+from bs4 import BeautifulSoup
 
 targetUrl="https://github.com/trending"
 r = requests.get(targetUrl, verify=True)
-print(r.text)
+soup=BeautifulSoup(r.text)
+
+ol=soup.find_all("ol", class_="repo-list")
+print(ol[0])
+
