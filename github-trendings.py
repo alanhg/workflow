e778f61fa3be7510d1081432648cf78ba8469b39
alanhg <qianghe421@163.com>
2017-08-08 22:51:50 +0800

    update

--- github-trendings.py
+++ github-trendings.py
@@ -5,11 +5,17 @@
 import requests
 from bs4 import BeautifulSoup
 
+from sendEmail import EMailSender
+
 targetUrl = "https://github.com/trending"
 r = requests.get(targetUrl, verify=True)
 soup = BeautifulSoup(r.text, "html.parser")
-olObj = soup.find('ol', class_="repo-list")
+olObj = soup.find('ol', class_="repo-list").find_all("li")
 
-for liObj in olObj.children:
-    print(liObj)
-# .div.h3.a.span.string
+print('热门仓库')
+for liObj in olObj:
+    print(liObj.div.a)
+
+emailSender=EMailSender()
+
+emailSender.send(str(olObj))
